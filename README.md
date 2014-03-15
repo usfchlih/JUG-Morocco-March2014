dd43eaefe6ea2c101fa000bed421e9c273161ba7
Zak Elfassi <zakelfassi@gmail.com>
2014-03-15 00:33:19 +0000

    Yo

---
+++ README.md
@@ -0,0 +1,10 @@
+What Does The Web Look Like in 2014?
+=============================================================
+
+## About ##
+A talk given at a JUG Morocco meeting in March2014.
+
+Available for preview at : http://add-later.github.com
+
+## Talk code ##
+/examples/*
---
+++ slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'What does the Web look like in 2014?',
+    subtitle: 'Layouts, UI Design, and more!',
+    useBuilds: true, // False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled.
+    //analytics: 'UA-XXXXXXXX-1', // Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+    //theme: ['mytheme'], // Add themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Zak El Fassi',
+    company: 'Xoogler Web Developer @ Google / Founder @ Zaigood',
+    gplus: 'http://google.com/+ZakElFassi',
+    twitter: '@zakelfassi',
+    email : 'z@kelfassi.com',
+    github: 'http://github.com/zakelfassi'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
